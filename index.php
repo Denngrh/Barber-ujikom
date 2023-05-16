07d50a08e0c453961901e8b6522deaf34c31b93f
Denngrh <khansa0201ab@gmail.com>
2023-05-16 19:57:52 +0700

    update index

--- index.php
+++ index.php
@@ -17,58 +17,9 @@
     <title>Lily Putra</title>
   </head>
   <body>
-     <!-- Navbar Start -->
-<nav class="navbar navbar-expand-lg navbar-light bg-light p-3">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">
-          <img src="./assets/img/logo.png" alt="Logo" width="38" height="35"
-        /></a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNavAltMarkup"
-          aria-controls="navbarNavAltMarkup"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div class="navbar-nav ms-auto">
-            <li class="nav-item">
-              <a
-                href="./index.php"
-                class="nav-link btn btn-outline-theme ps-3 pe-3"
-                >Home</a
-              >
-            </li>
-            <li class="nav-item">
-              <a
-                href="#service"
-                class="nav-link btn btn-outline-theme ps-3 pe-3"
-                >home</a 
-              >
-            </li>
-            <li class="nav-item">
-              <a
-                href="#contact"
-                class="nav-link btn btn-outline-theme ps-3 pe-3"
-                >home</a
-              >
-            </li>
-            <li class="nav-item">
-              <a
-                href="#faq"
-                class="nav-link btn btn-outline-theme ps-3 pe-3"
-                >home</a
-              >
-            </li>
-          </div>
-        </div>
-      </div>
-    </nav>
-    <!-- Navbar End -->
+      <!-- navbar -->
+      <?php include'inc/header.php'; ?>
+        <!-- navbar end -->
 
         <!-- Scroll -->
         <button class="go-top-btn">
@@ -103,7 +54,10 @@
       </div>
     </section>
     <!-- Banner End -->
-    
+
+        <!-- Footer -->
+        <?php include'inc/footer.php'; ?>
+        <!-- Footer end -->
     <script
       src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
       integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
