5816699e621ec4efe16881f88b235161c5747b66
Denngrh <khansa0201ab@gmail.com>
2023-05-16 16:35:30 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+      crossorigin="anonymous"
+    />
+    <link rel="stylesheet" href="assets/style.css" />
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+    <link rel="shorcut icon" type="image/png" href="assets/img/logo.png" />
+    <script src="assets/sciript.js" defer></script>
+    <title>Lily Putra</title>
+  </head>
+  <body>
+     <!-- Navbar Start -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light p-3">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          <img src="./assets/img/logo.png" alt="Logo" width="38" height="35"
+        /></a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav ms-auto">
+            <li class="nav-item">
+              <a
+                href="./index.php"
+                class="nav-link btn btn-outline-theme ps-3 pe-3"
+                >Home</a
+              >
+            </li>
+            <li class="nav-item">
+              <a
+                href="#service"
+                class="nav-link btn btn-outline-theme ps-3 pe-3"
+                >home</a 
+              >
+            </li>
+            <li class="nav-item">
+              <a
+                href="#contact"
+                class="nav-link btn btn-outline-theme ps-3 pe-3"
+                >home</a
+              >
+            </li>
+            <li class="nav-item">
+              <a
+                href="#faq"
+                class="nav-link btn btn-outline-theme ps-3 pe-3"
+                >home</a
+              >
+            </li>
+          </div>
+        </div>
+      </div>
+    </nav>
+    <!-- Navbar End -->
+
+        <!-- Scroll -->
+        <button class="go-top-btn">
+        <img src="assets/img/arrow-up.png" alt="arrow up">
+        </button>
+       <!-- End Scroll -->
+
+    <!-- Banner Start -->
+    <section class="hero container">
+      <div class="mt-5">
+        <div class="row ms-3">
+          <div class="col-md-6">
+            <h1 class="fw-bold">
+              Lily Putra <br />
+              Barber
+            </h1>
+            <p class="text-muted mt-3">
+            Temukan tukang cukur profesional yang membuat rambut Anda terlihat
+              <div id="typing">Tampan dan Rapi</div>
+             <div id="line">|</div>
+            </p>
+          </div>
+          <div class="col-md-6 mt-2">
+            <img
+              class="img-banner img-fluid"
+              src="assets/img/banner.svg"
+              alt="barber logo"
+              width="500px"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Banner End -->
+    
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+      crossorigin="anonymous"
+    ></script>
+    <script src="https://unpkg.com/feather-icons"></script>
+    <script>
+      feather.replace();
+    </script>
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+    <script>
+      AOS.init();
+    </script>
+  </body>
+</html>
