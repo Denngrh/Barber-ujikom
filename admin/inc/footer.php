2ec5953552bcfcfb546129a2d1c2080424170c87
Denngrh <khansa0201ab@gmail.com>
2023-05-20 18:30:01 +0700

    include

---
+++ admin/inc/footer.php
@@ -0,0 +1,10 @@
+ <!-- ======= Footer ======= -->
+ <footer id="footer" class="footer">
+    <div class="copyright">
+      &copy; Copyright <strong><span>2023</span></strong>. All Rights Reserved
+    </div>
+    <div class="credits">
+        <strong>Denngrh | Develop</strong>
+    </div>
+  </footer>
+  <!-- End Footer -->
