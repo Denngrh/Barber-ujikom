13cba045187172ae55bc1265196774b15df49f13
Denngrh <khansa0201ab@gmail.com>
2023-05-29 08:21:07 +0700

    update file

--- user/inc/footer.php
+++ user/inc/footer.php
@@ -16,15 +16,15 @@
             <h6 class="fw-bold">tautan langsung</h6>
             <ul class="list-group list-unstyled">
             <li class="pb-2"><a href="#contact" class="text-muted text-decoration-none">Kontak</a></li>
-            <li class="pb-2"><a href="#faq" class="text-muted text-decoration-none">F.A.Q</a></li>
-            <li class="pb-2"><a href="#location" class="text-muted text-decoration-none">Lokasi</a></li> 
+            <li class="pb-2"><a href="#invoices" class="text-muted text-decoration-none">Invoice</a></li>
+            <li class="pb-2"><a href="#profile" class="text-muted text-decoration-none">profile</a></li> 
             </ul>
           </div>
           <div class="col-md-2 mb-3">
             <h6 class="fw-bold">Beberapa tautan</h6>
             <ul class="list-group list-unstyled">
-              <li class="pb-2"><a href="./error/error.html" class="text-muted text-decoration-none">Cookie Police</a></li>
-              <li class="pb-2"><a href="./error/error.html" class="text-muted text-decoration-none">Support</a></li>
+              <li class="pb-2"><a href="404.php" class="text-muted text-decoration-none">Cookie Police</a></li>
+              <li class="pb-2"><a href="404.php" class="text-muted text-decoration-none">Support</a></li>
             </ul>
           </div>
           <div class="col-md-2">
